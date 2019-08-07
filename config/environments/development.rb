5ba36d5e20fbf75e886365763e70c10069b27515
Vladimir Sharay <volodya22all@gmail.com>
2019-08-07 13:38:45 +0300

    Do env variables through figaro

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,14 +60,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { host: ENV["MAIL_HOST"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'smtp.gmail.com',
-    port:          '587',
+    user_name:      ENV["SENDMAIL_USERNAME"],
+    password:       ENV["SENDMAIL_PASSWORD"],
+    domain:         ENV["MAIL_HOST"],
+    address:       "smtp.gmail.com",
+    port:          "587",
     authentication: :plain,
     enable_starttls_auto: true
   }
