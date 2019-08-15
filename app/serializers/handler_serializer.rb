ad9c3de10bc3871a983e21d9f1e830ae3ad27fe5
Your Name <you@example.com>
2019-08-15 14:00:47 +0300

    Add avatar_url

--- app/serializers/handler_serializer.rb
+++ app/serializers/handler_serializer.rb
@@ -1,7 +1,13 @@
 class HandlerSerializer
   include FastJsonapi::ObjectSerializer
+  include AvatarHelper
   attributes :name, :metro
   attribute :trip_count do |handler|
     handler.trips.count
   end
+
+  attribute :avatar_url do |handler|
+    avatar_url(handler)
+  end
+
 end
