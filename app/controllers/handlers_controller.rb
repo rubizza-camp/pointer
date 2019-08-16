30619bdd5edd58cfb85b57937627a0354eb2d0ed
Your Name <you@example.com>
2019-08-16 09:16:47 +0300

    Add second serializer

--- app/controllers/handlers_controller.rb
+++ app/controllers/handlers_controller.rb
@@ -1,9 +1,12 @@
 class HandlersController < ApplicationController
   def index
     respond_to do |format|
-      format.html {render :index}# index.html.erb
-      format.json { render json: HandlerSerializer.new(Handler.all).serialized_json}
+      format.html { render :index } # index.html.erb
+      format.json { render json: HandlerSerializer.new(Handler.all).serialized_json }
     end
+  end
 
+  def edit
+    render json: HandlerProfileSerializer.new(Handler.all).serialized_json
   end
 end
---
+++ app/serializers/handler_profile_serializer.rb
@@ -0,0 +1,13 @@
+module HandlerProfileSerializer
+  include FastJsonapi::ObjectSerializer
+  include AvatarHelper
+  attributes :name, :metro, :description
+  attribute :trip_count do |handler|
+    handler.trips.count
+  end
+
+  attribute :avatar_url do |handler|
+    avatar_url(handler)
+  end
+
+end
