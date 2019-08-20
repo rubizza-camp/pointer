cde8405e1729c1fb7ad056012e75fea7ccc91746
valkovik15 <kopotchel@outlook.com>
2019-08-20 17:33:12 +0300

    Change makeNum signature

--- app/webpacker/utils/map_helpers.js
+++ app/webpacker/utils/map_helpers.js
@@ -1,3 +1,3 @@
-export const makeNum = (lat, lng) => ({ lat: Number(lat), lng: Number(lng) })
+export const makeNum = ({ lat, lng }) => ({ lat: Number(lat), lng: Number(lng) })
 
-export const makeNums = arr => arr.map(({ attributes }) => makeNum(attributes.lat, attributes.lng))
+export const makeNums = arr => arr.map(({ attributes }) => makeNum(attributes))
