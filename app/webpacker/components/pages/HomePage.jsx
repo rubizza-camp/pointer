9b60e5529c93ac1eb2359e59ff8796f63bf506b0
Vadim Hvistik <hvistikvadim@gmail.com>
2019-08-27 20:25:22 +0300

    Implement sample local toggle

--- app/webpacker/components/pages/HomePage.jsx
+++ app/webpacker/components/pages/HomePage.jsx
@@ -5,12 +5,14 @@ import Request from '../items/Request'
 import Questions from '../items/Questions'
 import Reviews from '../items/Reviews'
 import Members from '../items/Members'
+import I18n from 'i18n-js'
 
 function HomePage() {
   return (
     <>
       <Banner />
       <Container>
+        <h1>{I18n.t('hello')}</h1>
         <Request />
         <Questions />
         <Members />
--- app/webpacker/packs/example.jsx
+++ app/webpacker/packs/example.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import I18n from 'i18n-js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from '../components/items/Header'
 import Footer from '../components/items/Footer'
 import MembersPage from '../components/pages/MembersPage'
 import HomePage from '../components/pages/HomePage'
-import './i18n/translations'
 
 global.I18n = I18n
 
-I18n.translations = {
-  en: {
-    hello: 'Hi!',
-  },
-}
+require('./i18n/translations')
 
-function Basic() {
-  return (
-    <>
-      <h1>{I18n.t('hello')}</h1>
-      <Router>
-        <Header />
-        <Route path="/" exact component={HomePage} />
-        <Route path="/members" component={MembersPage} />
-      </Router>
-      <Footer />
-    </>
-  )
+class Basic extends Component {
+  state = { locale: 'en' }
+
+  changeLocale = () => {
+    I18n.locale = 'ru'
+    this.setState(() => ({ locale: 'ru' }))
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.changeLocale}>RUSSIAN!!!</button>
+        <h1>{I18n.t('hello')}</h1>
+        <Router>
+          <Header />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/members" component={MembersPage} />
+        </Router>
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default Basic
