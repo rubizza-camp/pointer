c3da5bef7639ae8c59fde58b5226ed0f9272173a
showthreadx <showthreadx@gmail.com>
2019-08-29 02:36:13 +0300

    Fixed warnings and added PropTypes

--- app/webpacker/components/items/signin/Errors.jsx
+++ app/webpacker/components/items/signin/Errors.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Alert } from 'reactstrap'
 
 const Errors = (props) => {
-  if (!props.data) {
-    return null
-  }
+  if (!props.data) return null
 
   return (
     <div>
       <Alert color="danger">
-        <h3>Invalid email fo password</h3>
+        <h3>Invalid email or password</h3>
       </Alert>
     </div>
   )
+}
+
+Errors.propTypes = {
+  data: PropTypes.object.isRequired,
 }
 
 export default Errors
--- app/webpacker/components/items/signup/Errors.jsx
+++ app/webpacker/components/items/signup/Errors.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Alert } from 'reactstrap'
 
 const Errors = (props) => {
-  if (!props.data.error) {
-    return null
-  }
+  if (!props.data.error) return null
 
   const arr = props.data.error.split('<br>')
   const items = arr.map((error) => <b><li>{error}</li></b>)
@@ -15,6 +14,10 @@ const Errors = (props) => {
       </Alert>
     </div>
   )
+}
+
+Errors.propTypes = {
+  data: PropTypes.object.isRequired,
 }
 
 export default Errors
