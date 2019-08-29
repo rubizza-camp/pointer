54faa9684d77ec7fed11248cc9e691864716d5b4
showthreadx <showthreadx@gmail.com>
2019-08-29 18:06:31 +0300

    Fixed hound

--- app/webpacker/components/items/signin/Errors.jsx
+++ app/webpacker/components/items/signin/Errors.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Alert } from 'reactstrap'
 
-const Errors = (props) => {
-  if (!props.data) return null
-  console.log(props.data.error.error)
+const Errors = ({ data }) => {
+  if (!data) return null
+  console.log(data.error.error)
 
   return (
     <div>
       <Alert color="danger">
-        <h5>{props.data.error.error}</h5>
+        <h5>{data.error.error}</h5>
       </Alert>
     </div>
   )
+}
+
+Errors.propTypes = {
+  data: PropTypes.shape({
+    error: PropTypes.string,
+  }).isRequired,
 }
 
 export default Errors
