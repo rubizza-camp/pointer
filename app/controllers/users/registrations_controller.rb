564188b0e832aa60ef84ffcb8054e1ee5a87e05d
showthreadx <showthreadx@gmail.com>
2019-08-30 13:28:13 +0300

    Fixed CSRF-Token Header in requests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
   respond_to :json
 
-  # POST /resource
-   def create    
+  #POST /resource
+  def create
     build_resource(sign_up_params)
 
     resource.save
@@ -15,7 +14,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         msg = find_message(:signed_up, {})
         sign_up(resource_name, resource)
         respond_with(resource) do |format|
-          format.json { render json: {url: after_sign_up_path_for(resource)}, status: 200 }
+        format.json { render json: {url: after_sign_up_path_for(resource)}, status: 200 }
         end
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
@@ -35,6 +34,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
         format.json { render json: {message: msg}, status: 401 }
       end
     end
-   end
-   
+  end
 end
